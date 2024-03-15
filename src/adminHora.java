8f3d0c79781ae3bea3809c5ac8e34d8efe922e0c
amill <amillaguevara@gmail.com>
2024-03-15 13:31:21 -0600

    Creacion del thread de la hora

--- src/adminHora.java
+++ src/adminHora.java
@@ -1,12 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author amill
- */
-public class adminHora {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JLabel;
+
+public class adminHora extends Thread{
+    private JLabel hora;
     
+    public adminHora(JLabel hora) {
+        this.hora = hora;
+    }        
+
+    @Override
+    public void run() {
+        while(true){
+            Date h=new Date();
+            DateFormat f=new SimpleDateFormat("hh:mm:ss");
+            hora.setText( f.format(h) );
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+                
+            }
+        }        
+    }
 }
